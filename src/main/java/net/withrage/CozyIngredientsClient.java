b4c5983ee84240b28e31642290a3ad81e1d53cca
justeriksk <sebo.erik@gmail.com>
2025-07-13 21:15:03 +0200

    Initial commit

---
+++ src/main/java/net/withrage/CozyIngredientsClient.java
@@ -0,0 +1,10 @@
+package net.withrage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CozyIngredientsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
